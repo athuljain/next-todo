f710e46d760a453f28574352b4dd86f2173b292a
athuljain <athuljainkj2@gmail.com>
2025-10-15 14:10:30 +0530

    next-todo connect mongodb

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,21 +1,18 @@
-import { log } from "console";
 import mongoose from "mongoose";
 
-const MONGODB_URI=process.env.MONGODB_URI;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if(!MONGODB_URI) throw new Error("please define MONGODB_URI in .env.local")
+if (!MONGODB_URI) throw new Error("Please define MONGODB_URI in .env.local");
 
-    let isConnected=false
+let isConnected = false;
 
-    export const connectDB=async()={
-        if(isConnected) return;
-        try{
-            await mongoose.connect(MONGODB_URI)
-            isConnected=true;
-            console.log("MongoDB Connected");
-
-            
-        }catch (error){
-            console.error("MongoDB connection error :",error)
-        }
-    }
+export const connectDB = async () => {
+  if (isConnected) return;
+  try {
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true;
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error);
+  }
+};
